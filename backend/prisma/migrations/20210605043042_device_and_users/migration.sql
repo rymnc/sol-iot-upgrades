6a3ab90e230d99eea0269df849a524aaae3f9045
rymnc <aaryamannchallani7@gmail.com>
2021-06-05 10:40:17 +0530

    feat: tests, schema update

---
+++ backend/prisma/migrations/20210605043042_device_and_users/migration.sql
@@ -0,0 +1,36 @@
+/*
+  Warnings:
+
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "User";
+
+-- CreateTable
+CREATE TABLE "Users" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Devices" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "ipfsHash" TEXT NOT NULL,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Users.email_unique" ON "Users"("email");
+
+-- AddForeignKey
+ALTER TABLE "Devices" ADD FOREIGN KEY ("userId") REFERENCES "Users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- backend/src/routes/pinata/index.ts
+++ backend/src/routes/pinata/index.ts
@@ -3,6 +3,7 @@ import { FastifyPluginAsync } from "fastify";
 const pinata: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   fastify.get("/health", async function (request, reply) {
     const auth = await fastify.pinata.testAuthentication();
+    console.log(auth);
     reply.status(200).send(auth);
   });
 };
